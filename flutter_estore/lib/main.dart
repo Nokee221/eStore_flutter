6e403078c2667de713a2d0e9bf5cb55b8f096ee5
Kenan Cosic <45003119+Nokee221@users.noreply.github.com>
2021-12-27 14:38:36 +0100

    Update UserScreen,add ThemeSwitch

--- flutter_estore/lib/main.dart
+++ flutter_estore/lib/main.dart
@@ -1,32 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_estore/consts/theme.dart';
+import 'package:flutter_estore/provider/dark_theme_provider.dart';
 import 'package:flutter_estore/screens/bottom_bar.dart';
 import 'package:flutter_estore/screens/cart.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key); 
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DarkThemeProvider themeChangeProvider = DarkThemeProvider();
+
+  void getCurrentAppTheme() async{
+    themeChangeProvider.darkTheme = await themeChangeProvider.darkThemePreferences.getTheme();
+  }
+
+  @override
+  void initState() {
+    getCurrentAppTheme();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: BottomBarScreen(),
-    );
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) {
+            return themeChangeProvider;
+          },
+        )
+      ],
+    
+    child: Consumer<DarkThemeProvider>( 
+      builder: (context, themeData , child) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          theme: Styles.themeData(themeChangeProvider.darkTheme , context),
+          home: BottomBarScreen(),
+        );
+      }
+    ),);
   }
 }
--- flutter_estore/lib/screens/user_info.dart
+++ flutter_estore/lib/screens/user_info.dart
@@ -1,27 +1,195 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_estore/consts/colors.dart';
+import 'package:flutter_estore/provider/dark_theme_provider.dart';
+import 'package:list_tile_switch/list_tile_switch.dart';
+import 'package:provider/provider.dart';
 
-class UserInfo extends StatelessWidget {
-  UserInfo({ Key? key }) : super(key: key);
+class UserInfo extends StatefulWidget {
+  UserInfo({Key? key}) : super(key: key);
+
+  @override
+  State<UserInfo> createState() => _UserInfoState();
+}
+
+class _UserInfoState extends State<UserInfo> {
+  late ScrollController _scrollController = new ScrollController();
+  var top = 0.0;
 
   @override
   Widget build(BuildContext context) {
+  final themeChenge = Provider.of<DarkThemeProvider>(context);
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 8.0),
-            child: userTitle("User info"),
+          CustomScrollView(
+            controller: _scrollController,
+            slivers: <Widget>[
+              SliverAppBar(
+                automaticallyImplyLeading: false,
+                elevation: 4,
+                expandedHeight: 300,
+                pinned: true,
+                flexibleSpace: LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints contraints) {
+                    top = contraints.biggest.height;
+                    return Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Colors.blue,
+                            ColorsConsts.endColor,
+                          ],
+                          begin: const FractionalOffset(0.0, 0.0),
+                          end: const FractionalOffset(1.0, 0.0),
+                          stops: [0.0, 1.0],
+                          tileMode: TileMode.clamp,
+                        ),
+                      ),
+                      child: FlexibleSpaceBar(
+                        collapseMode: CollapseMode.parallax,
+                        centerTitle: true,
+                        title: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            AnimatedOpacity(
+                              duration: Duration(milliseconds: 300),
+                              opacity: top <= 110.0 ? 1.0 : 0,
+                              child: Row(
+                                children: [
+                                  SizedBox(
+                                    width: 12,
+                                  ),
+                                  Container(
+                                    height: kToolbarHeight / 1.8,
+                                    width: kToolbarHeight / 1.8,
+                                    decoration: BoxDecoration(
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.white,
+                                          blurRadius: 1.0,
+                                        ),
+                                      ],
+                                      shape: BoxShape.circle,
+                                      image: DecorationImage(
+                                        fit: BoxFit.fill,
+                                        image: NetworkImage(
+                                            'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png'),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 12,
+                                  ),
+                                  Text(
+                                    "Guest",
+                                    style: TextStyle(
+                                      fontSize: 20.0,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        background: Image(
+                          image: NetworkImage(
+                              'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png'),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: userTitle("User info"),
+                    ),
+                    Divider(
+                      thickness: 1,
+                      color: Colors.grey,
+                    ),
+                    userListTile('Email', 'Your Email', 0, context),
+                    userListTile('Phone', 'Your Phone', 1, context),
+                    userListTile('Shipping Address', 'Your address', 2, context),
+                    userListTile('Joined Date', 'Date ', 3, context),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: userTitle("User settings"),
+                    ),
+                    Divider(
+                      thickness: 1,
+                      color: Colors.grey,
+                    ),
+                    ListTileSwitch(
+                      value: themeChenge.darkTheme,
+                      leading: Icon(Icons.mood_rounded),
+                      onChanged: (value) {
+                        setState(() {
+                          themeChenge.darkTheme = value;
+                        });
+                      },
+                      visualDensity: VisualDensity.comfortable,
+                      switchType: SwitchType.cupertino,
+                      switchActiveColor: Colors.indigo,
+                      title: Text('Dark theme'),
+                    ),
+                    userListTile('Logout', ' ', 4, context),
+                  ],
+                ),
+              ),
+            ],
           ),
-          Divider(thickness: 1, color: Colors.grey,),
-          userListTile('Email' , 'Your Email' , 0 , context),
-          userListTile('Phone' , 'Your Phone' , 1 , context),
-          userListTile('Shipping Address' , 'Your address' , 2 , context),
-          userListTile('Joined Date' , 'Date ' , 3 , context),
+         // _buildFab(),
         ],
-      )
-      
+      ),
+    );
+  }
+
+  Widget _buildFab() {
+    //starting fab position
+    final double defaultTopMargin = 300.0 - 4.0;
+    //pixels from top where scaling should start
+    final double scaleStart = 160.0;
+    //pixels from top where scaling should end
+    final double scaleEnd = scaleStart / 2;
+
+    double top = defaultTopMargin;
+    double scale = 1.0;
+    if (_scrollController.hasClients) {
+      double offset = _scrollController.offset;
+      top -= offset;
+      if (offset < defaultTopMargin - scaleStart) {
+        //offset small => don't scale down
+        scale = 1.0;
+      } else if (offset < defaultTopMargin - scaleEnd) {
+        //offset between scaleStart and scaleEnd => scale down
+        scale = (defaultTopMargin - scaleEnd - offset) / scaleEnd;
+      } else {
+        //offset passed scaleEnd => hide fab
+        scale = 0.0;
+      }
+    }
+
+    return Positioned(
+      top: top,
+      right: 16.0,
+      child: Transform(
+        transform: Matrix4.identity()..scale(scale),
+        alignment: Alignment.center,
+        child: FloatingActionButton(
+          backgroundColor: Colors.purple,
+          heroTag: "btn1",
+          onPressed: () {},
+          child: Icon(Icons.camera_alt_outlined),
+        ),
+      ),
     );
   }
 
@@ -33,31 +201,29 @@ class UserInfo extends StatelessWidget {
     Icons.exit_to_app_rounded,
   ];
 
-  Widget userListTile(String title,String subTitles, int index,  BuildContext context)
-  {
+  Widget userListTile(
+      String title, String subTitles, int index, BuildContext context) {
     return Material(
       color: Colors.transparent,
       child: InkWell(
-          splashColor: Theme.of(context).splashColor ,
-          child: ListTile(
-            onTap: (){},
-            title: Text(title),
-            subtitle: Text(subTitles),
-            leading: Icon(_userTileIcons[index]),
-          ),
+        splashColor: Theme.of(context).splashColor,
+        child: ListTile(
+          onTap: () {},
+          title: Text(title),
+          subtitle: Text(subTitles),
+          leading: Icon(_userTileIcons[index]),
         ),
+      ),
     );
   }
 
-  Widget userTitle(String title)
-  {
-    return 
-    Text(
-      title,
-      style: TextStyle(
-      fontWeight: FontWeight.bold,
-      fontSize: 23,
-      color: Colors.black
+  Widget userTitle(String title) {
+    return Padding(
+      padding: const EdgeInsets.all(14.0),
+      child: Text(
+        title,
+        style: const TextStyle(
+            fontWeight: FontWeight.bold, fontSize: 23, color: Colors.black),
       ),
     );
   }
