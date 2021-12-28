f3d9df9ba3f7183e68477e620ec6233d7f25a34e
Kenan Cosic <45003119+Nokee221@users.noreply.github.com>
2021-12-29 00:01:18 +0100

    Update Cart and FullCartPage

--- flutter_estore/lib/screens/cart.dart
+++ flutter_estore/lib/screens/cart.dart
@@ -1,13 +1,102 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_estore/screens/cart_empty.dart';
+import 'package:flutter_estore/screens/cart_full.dart';
 
 class CartPage extends StatelessWidget {
-  const CartPage({ Key? key }) : super(key: key);
+  const CartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CartEmpty(),
-    );
+    List products = [];
+    return !products.isEmpty
+        ? Scaffold(body: CartEmpty())
+        : Scaffold(
+            bottomSheet: checkoutSection(context),
+            appBar: AppBar(
+              title: Text(
+                'Cart Items Count',
+              ),
+              actions: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.delete,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            body: Container(
+              margin: EdgeInsets.only(bottom: 60),
+              child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (BuildContext ctx, int index) {
+                  return CartFull();
+                },
+              ),
+            ),
+          );
+  }
+
+  Widget checkoutSection(BuildContext ctx) {
+    return Container(
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(
+              color: Colors.grey,
+              width: 0.5,
+            ),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Material(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.red,
+                  child: InkWell(
+                    onTap: () {},
+                    borderRadius: BorderRadius.circular(30),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Checkout',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Theme.of(ctx).textSelectionColor,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Spacer(),
+              Text(
+                'Total:',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Theme.of(ctx).textSelectionColor,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(width: 5),
+              Text(
+                'US \$1300',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
---
+++ flutter_estore/lib/screens/cart_full.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_estore/consts/colors.dart';
+import 'package:flutter_estore/provider/dark_theme_provider.dart';
+import 'package:provider/provider.dart';
+
+class CartFull extends StatefulWidget {
+  const CartFull({Key? key}) : super(key: key);
+
+  @override
+  _CartFullState createState() => _CartFullState();
+}
+
+class _CartFullState extends State<CartFull> {
+  @override
+  Widget build(BuildContext context) {
+    final themeChange = Provider.of<DarkThemeProvider>(context);
+
+    return Container(
+      height: 140,
+      margin: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+            bottomRight: Radius.circular(16), topRight: Radius.circular(16)),
+        color: Theme.of(context).backgroundColor,
+      ),
+      child: Row(
+        children: [
+          Container(
+            width: 130,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                    'https://cdn.shopify.com/s/files/1/0024/9803/5810/products/530254-Product-1-I-637673295149200981_1080x.jpg'),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          SizedBox(width: 6),
+          Flexible(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Flexible(
+                      child: Text(
+                        "Iphone 13",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                    Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(32.0),
+                        onTap: () {},
+                        child: Container(
+                          height: 50,
+                          width: 50,
+                          child: Icon(
+                            Icons.delete,
+                            color: Colors.red,
+                            size: 22,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text("Price: "),
+                    SizedBox(width: 5),
+                    Text(
+                      "1300",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text("Sub Total: "),
+                    SizedBox(width: 5),
+                    Text(
+                      "1300",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: themeChange.darkTheme
+                            ? Colors.brown.shade900
+                            : Theme.of(context).accentColor,
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 6),
+                Row(
+                  children: [
+                    Text(
+                      "Ships Free",
+                      style: TextStyle(
+                        color: themeChange.darkTheme
+                            ? Colors.brown.shade900
+                            : Theme.of(context).accentColor,
+                      ),
+                    ),
+                    Spacer(),
+                    Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(4.0),
+                        onTap: () {},
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Container(
+                            child: Icon(
+                              Icons.exposure_minus_1,
+                              color: Colors.red,
+                              size: 22,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Card(
+                      elevation: 12,
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 0.11,
+                        padding: EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                          colors: [
+                            ColorsConsts.gradiendLStart,
+                            ColorsConsts.gradiendLEnd,
+                          ],
+                          stops: [0.0, 0.7],
+                        )),
+                        child: Text(
+                          "1",
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                    Material( 
+                      color: Colors.transparent,
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(4.0),
+                        onTap: () {},
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Container(
+                            child: Icon(
+                              Icons.plus_one,
+                              color: Colors.green,
+                              size: 22,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
