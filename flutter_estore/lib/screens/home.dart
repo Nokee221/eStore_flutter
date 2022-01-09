3de1df7e0c06cddbdf1e2e6a758870baac6b9ac4
Kenan Cosic <45003119+Nokee221@users.noreply.github.com>
2022-01-09 15:32:17 +0100

    Added HomeScreen with Carouse

--- flutter_estore/lib/screens/home.dart
+++ flutter_estore/lib/screens/home.dart
@@ -1,14 +1,116 @@
 import 'package:flutter/material.dart';
+import 'package:backdrop/backdrop.dart';
+import 'package:flutter_estore/consts/colors.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+
+final List<String> _carouselImages = [
+    'assets/images/carousel1.png',
+    'assets/images/carousel2.jpeg',
+    'assets/images/carousel3.jpg',
+    'assets/images/carousel4.png',
+  ];
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  
+
+  
+
+  List<int> list = [1,2,3,4];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Home"),
+        child: BackdropScaffold(
+          headerHeight: MediaQuery.of(context).size.height * 0.25,
+          appBar: BackdropAppBar(
+            title: Text("Home"),
+            leading: BackdropToggleButton(
+              icon: AnimatedIcons.home_menu,
+            ),
+            flexibleSpace: Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                ColorsConsts.starterColor,
+                ColorsConsts.endColor
+              ])),
+            ),
+            actions: <Widget>[
+              IconButton(
+                iconSize: 15,
+                padding: EdgeInsets.all(10),
+                icon: CircleAvatar(
+                  radius: 15,
+                  backgroundColor: Colors.white,
+                  child: CircleAvatar(
+                    radius: 13,
+                    backgroundImage: NetworkImage(
+                        'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png'),
+                  ),
+                ),
+                onPressed: () {},
+              )
+            ],
+          ),
+          backLayer: Center(
+            child: Text("Back Layer"),
+          ),
+          frontLayer: Container(
+            child: CarouselSlider(
+              options: CarouselOptions(
+                aspectRatio: 2.0,
+                enlargeCenterPage: true,
+                enableInfiniteScroll: false,
+                initialPage: 2,
+                autoPlay: true,
+              ),
+              items: imageSliders,
+            ),
+          ),
+        ),
       ),
     );
   }
+
+  final List<Widget> imageSliders = _carouselImages
+    .map((item) => Container(
+          child: Container(
+            margin: EdgeInsets.all(5.0),
+            child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                child: Stack(
+                  children: <Widget>[
+                    Image.asset(item, fit: BoxFit.fill, width: 1000.0),
+                    Positioned(
+                      bottom: 0.0,
+                      left: 0.0,
+                      right: 0.0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Color.fromARGB(200, 0, 0, 0),
+                              Color.fromARGB(0, 0, 0, 0)
+                            ],
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                          ),
+                        ),
+                        padding: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 20.0),
+                      ),
+                    ),
+                  ],
+                )),
+          ),
+        ))
+    .toList();
 }
